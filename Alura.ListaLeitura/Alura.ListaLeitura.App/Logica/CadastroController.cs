eb9cf38c62fc34f1cd9b851011b081baf69d69e3
Matheus <mathfrance@hotmail.com>
2021-03-15 23:22:35 -0300

    Final Projeto

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/CadastroController.cs
@@ -0,0 +1,23 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+    public class CadastroController
+    {
+        public static string Incluir(Livro livro)
+        {           
+            var _repo = new LivroRepositorioCSV();
+            _repo.Incluir(livro);
+            return "Livro Adicionado com sucesso";
+        }
+
+        public IActionResult ExibirFormulario()
+        {
+            var html = new ViewResult { ViewName = "formulario" };
+            return html;
+        }
+    }
+}
---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Logica/LivrosController.cs
@@ -0,0 +1,50 @@
+﻿using Alura.ListaLeitura.App.HTML;
+using Alura.ListaLeitura.App.Negocio;
+using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.Logica
+{
+   public class LivrosController : Controller
+    {
+        public static string Detalhes(int id)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var livroEncontrado = _repo.Todos.First(livro => livro.Id == id);
+
+            return livroEncontrado.Detalhes();
+        }        
+
+        private static string CarregaLista(IEnumerable<Livro> livros)
+        {
+            var conteudoArquivo = HtmlUtils.CarregaArquivoHTML("lista");           
+
+            return conteudoArquivo;
+        }
+
+        public IActionResult ParaLer()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.ParaLer.Livros;
+            return View("lista");
+        }
+
+        public IActionResult Lendo()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lendo.Livros;
+            return View("lista");
+        }
+
+        public IActionResult Lidos()
+        {
+            var _repo = new LivroRepositorioCSV();
+            ViewBag.Livros = _repo.Lidos.Livros;
+            return View("lista");
+        }
+    }
+}
