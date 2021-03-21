f6f6000a1243be48141883068c983e509a669b7f
Matheus <mathfrance@hotmail.com>
2021-03-21 03:57:27 -0300

    e-Commerce com MVC e EF Core
    Aulas 1-5

---
+++ E-Commerce Com MVC e EF Core/Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,34 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Produto>().HasKey(p => p.Id);
+
+            modelBuilder.Entity<Pedido>().HasKey(p => p.Id);
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Itens).WithOne(t => t.Pedido);
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Cadastro).WithOne(t => t.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(i => i.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(c => c.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+        }
+    }
+}
